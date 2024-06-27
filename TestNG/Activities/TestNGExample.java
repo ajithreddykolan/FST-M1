c710b8a22703e55ac7d958a13332adff2f2b413d
ajithreddykolan <ajithreddy.kolan@ibm.com>
2024-06-28 00:10:07 +0530

    Add files via upload

---
+++ TestNG/Activities/TestNGExample.java
@@ -0,0 +1,30 @@
+package examples;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class TestNGExample {
+  @Test
+  public void f() {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("first");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+}
